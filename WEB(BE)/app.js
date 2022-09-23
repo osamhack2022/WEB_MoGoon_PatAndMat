e03ebcff3afdb304817e13a3a0c089db24274e26
Everdu <64959010+kckc0608@users.noreply.github.com>
2022-09-23 05:44:50 +0000

    기본 서버 생성
    기본 nodejs 앱 생성

---
+++ WEB(BE)/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("hello world!");
+});
+
+app.get('/sayhi', (req, res) => {
+    res.send("hi");
+});
+
+app.listen(port, () => {
+    console.log('server is listening at ${port}');
+});
