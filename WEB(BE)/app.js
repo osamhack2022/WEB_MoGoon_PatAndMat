bb9a8763cc3c14fc7d70e7633965c0e0958f11b6
Everdu <64959010+kckc0608@users.noreply.github.com>
2022-09-25 05:48:18 +0000

    포트 수정, 콘솔 로깅 추가, 리드미 추가
    리액트 앱이 3000포트를 쓰고 있어서 노드 서버 포트 5000으로 바꿨습니다.
    노드 서버에 데이터 요청하면 응답할 때 응답한 데이터 콘솔로 로깅하게 했어요
    서버 실행방법에 대한 리드미도 추가했습니다

--- WEB(BE)/app.js
+++ WEB(BE)/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.get('/speciality/list', (req, res) => {
-    res.json([
+    const test_data = [
         {
             military_kind: '공군',
             class: '일반',
@@ -49,7 +49,10 @@ app.get('/speciality/list', (req, res) => {
             tags: ['교대근무', '휴가많음', '실내근무'],
             like: 3654,
         },
-    ]);
+    ];
+    
+    console.log('server sent ${test_data}');
+    res.json(test_data);
 });
 
 app.listen(port, () => {
