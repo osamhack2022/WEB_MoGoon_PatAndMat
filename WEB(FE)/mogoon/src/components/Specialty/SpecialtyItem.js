ae7e12205911e62970a6e3aaf2aa024045794249
WBCode3 <80467790+WBCode3@users.noreply.github.com>
2022-09-25 14:53:48 +0000

    Specialty update

--- WEB(FE)/mogoon/src/components/Specialty/SpecialtyItem.js
+++ WEB(FE)/mogoon/src/components/Specialty/SpecialtyItem.js
@@ -4,7 +4,7 @@ import React from 'react';
 let SpecialtyItem =(props)=>{
     return(
         <div className='specialtyitem'>
-            
+            <div className='spimg'></div>
         </div>
     );
 };
