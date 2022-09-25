9d761f9f43a16ce1694a4570af93326372db7808
WBCode3 <vnddnjf1234@naver.com>
2022-09-25 20:01:06 +0900

    Header update, Specialty create

--- WEB(FE)/mogoon/src/App.js
+++ WEB(FE)/mogoon/src/App.js
@@ -3,15 +3,20 @@ import { useState } from 'react';
 
 //components
 import Header from "./components/Header/Header"
+import Specialty from "./components/Specialty/Specialty"
 
 //css
 import './App.css';
 import './css/Header.css';
+import './css/Specialty.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className='container'>
+        <Specialty/>
+      </div>
     </div>
   );
 }
