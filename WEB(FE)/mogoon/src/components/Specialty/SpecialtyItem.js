d297cbf32c0a047ac4db08e0ae4840ce7c3f173f
WBCode3 <80467790+WBCode3@users.noreply.github.com>
2022-09-26 11:17:17 +0000

    Specialty Update

--- WEB(FE)/mogoon/src/components/Specialty/SpecialtyItem.js
+++ WEB(FE)/mogoon/src/components/Specialty/SpecialtyItem.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-
-let SpecialtyItem =(props)=>{
-    return(
+let SpecialtyItem = (props) => {
+    return (
         <div className='specialtyitem'>
-            <div className='spimg'></div>
+            <div className='spimg' />
+            <div className='spcontent'>
+                <div className='sptype'>
+                    <SpTypeItem type="육군" />
+                    <SpTypeItem type="해군" />
+                    <SpTypeItem type="공군" />
+                    <SpTypeItem type="해병대" />
+                    {/* 일반인지 전문특기병인지 */}
+                    <div>일반</div>
+                </div>
+                <div className='spname'>{props.name}</div>
+                <p className='spexplan'>Lorem ipsum dolor, sit amet consectetur</p>
+                <div className='sptag'>
+                    <SpTagItem tag="휴가많음"/>
+                    <SpTagItem tag="실내근무"/>
+                    <SpTagItem tag="교대근무"/>
+                    <img src="img/etc/saveOff.svg"/>
+                </div>
+            </div>
         </div>
+    );
+};
+
+let SpTypeItem = (props) => {
+    return (
+        //type 기본값을 이용해서 type별로 css 적용
+        <div>{props.type}</div>
+    );
+};
+
+let SpTagItem = (props) => {
+    return (
+        //type 기본값을 이용해서 tag별로 css 적용
+        <div>#{props.tag}</div>
     );
 };
 
