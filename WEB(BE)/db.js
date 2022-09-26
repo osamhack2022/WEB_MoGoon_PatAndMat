2a39f2749672096a8709e66c1e4ebb01d5e695be
Everdu <64959010+kckc0608@users.noreply.github.com>
2022-09-26 07:09:08 +0000

    보안을 위해 파이어베이스 정보 env 파일로 분리

---
+++ WEB(BE)/db.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+export const db = getFirestore(firebaseApp);
