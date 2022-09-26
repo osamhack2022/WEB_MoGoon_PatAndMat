41509bf156b90ffa026f4ea70815d4fcac19d367
Everdu <64959010+kckc0608@users.noreply.github.com>
2022-09-26 03:42:28 +0000

    Firebase FireStore 연동
    Firebase Firestore와 연동하는 코드를 추가하였습니다.
    하드코딩된 데이터가 아니라 DB에서 데이터를 불러와 보내줍니다.

--- WEB(BE)/app.js
+++ WEB(BE)/app.js
@@ -1,58 +1,34 @@
-const express = require('express');
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, doc, setDoc } from 'firebase/firestore/lite';
+import express from 'express';
+
 const app = express();
 const port = 5000;
 
-app.get('/speciality/list', (req, res) => {
-    const test_data = [
-        {
-            military_kind: '공군',
-            class: '일반',
-            kind: '화생방',
-            speciality_name: '화생방',
-            speciality_code: '16',
-            tags: ['교대근무', '휴가많음', '실내근무'],
-            like: 3654,
-        },
-        {
-            military_kind: '공군',
-            class: '일반',
-            kind: '화생방',
-            speciality_name: '화생방',
-            speciality_code: '16',
-            tags: ['교대근무', '휴가많음', '실내근무'],
-            like: 3654,
-        },
-        {
-            military_kind: '공군',
-            class: '일반',
-            kind: '화생방',
-            speciality_name: '화생방',
-            speciality_code: '16',
-            tags: ['교대근무', '휴가많음', '실내근무'],
-            like: 3654,
-        },
-        {
-            military_kind: '공군',
-            class: '일반',
-            kind: '화생방',
-            speciality_name: '화생방',
-            speciality_code: '16',
-            tags: ['교대근무', '휴가많음', '실내근무'],
-            like: 3654,
-        },
-        {
-            military_kind: '공군',
-            class: '일반',
-            kind: '화생방',
-            speciality_name: '화생방',
-            speciality_code: '16',
-            tags: ['교대근무', '휴가많음', '실내근무'],
-            like: 3654,
-        },
-    ];
+const firebaseConfig = {
+    apiKey: "AIzaSyCCH8iBQRrh9RIk3BSSdmr9akG6znyrb8k",
+    authDomain: "osam--mogoon.firebaseapp.com",
+    projectId: "osam--mogoon",
+    storageBucket: "osam--mogoon.appspot.com",
+    messagingSenderId: "39262750716",
+    appId: "1:39262750716:web:2ba69ad64a14b138f39479",
+    measurementId: "G-79CB1TP6W3"
+};
 
-    console.log(`server sent ${test_data}`);
-    res.json(test_data);
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+app.get('/', async (req, res) => {
+    return res.send('hi');
+});
+
+app.get('/speciality/list', async (req, res) => {
+    const test_col = collection(db, 'speciality');
+    const snapshot = await getDocs(test_col);
+    const doc_list = snapshot.docs.map(doc => doc.data());
+    
+    await setDoc(doc(db, 'speciality', `test${doc_list.length}`), doc_list[0]);
+    return res.json(doc_list);
 });
 
 app.listen(port, () => {
