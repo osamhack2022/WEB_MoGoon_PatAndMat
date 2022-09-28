e9612f297b5cd40d338c8383ffb0cac853bd2881
Everdu <64959010+kckc0608@users.noreply.github.com>
2022-09-28 02:59:58 +0000

    라우팅 분리
    app.js에 모여있는 라우팅 코드를 분리했습니다.

---
+++ WEB(BE)/router/speciality.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { collection, getDocs, doc, setDoc } from 'firebase/firestore/lite';
+import { db } from '../db/db.js';
+
+export const router_speciality = express.Router();
+
+router_speciality.get('/list', async (req, res) => {
+    const test_col = collection(db, 'speciality');
+    const snapshot = await getDocs(test_col);
+    const doc_list = snapshot.docs.map(doc => doc.data());
+    
+    //await setDoc(doc(db, 'speciality', `test${doc_list.length}`), doc_list[0]);
+    return res.json(doc_list);
+});
+
+router_speciality.get('/list')
