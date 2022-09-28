8d26e791323af188630b4cdc1ce5508dc2fd97a0
WBCode3 <80467790+WBCode3@users.noreply.github.com>
2022-09-28 13:50:00 +0000

    Specialty grid update, Header update

--- WEB(FE)/mogoon/src/components/Header/Header.js
+++ WEB(FE)/mogoon/src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import HeaderItem from "./HeaderItem";
+import Button from '@mui/material/Button';
 
 let Header = (props) => {
     return (
@@ -11,6 +12,8 @@ let Header = (props) => {
                     <HeaderItem title="군지원" />
                     <HeaderItem title="커뮤니티" />
                     <HeaderItem title="마이페이지" />
+                    <Button variant="contained" className='btnLogin'>로그인</Button>
+                    <Button variant="outlined" className='btnJoin'>회원가입</Button>
                 </ul>
             </nav>
         </div>
