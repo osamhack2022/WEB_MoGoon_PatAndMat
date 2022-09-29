869f3ca1f6c53c001e2fe2972663a45687cec751
WBCode3 <80467790+WBCode3@users.noreply.github.com>
2022-09-29 03:50:02 +0000

    Specialty Filter create

--- WEB(FE)/mogoon/src/components/Specialty/Specialty.js
+++ WEB(FE)/mogoon/src/components/Specialty/Specialty.js
@@ -1,7 +1,13 @@
 // import React, { useState } from 'react';
 import React, { useEffect, useState } from "react";
 import SpecialtyItem from "./SpecialtyItem";
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 import axios from 'axios';
+import Typography from '@mui/material/Typography';
+import { makeStyles } from "@material-ui/styles";
+import { fontSize } from "@mui/system";
 
 const Specialty = (props) => {
     const [SpData, setSpData] = useState(null);
@@ -29,6 +35,9 @@ const Specialty = (props) => {
                 <input type={'text'} placeholder="특기를 입력해주세요."></input>
                 <div className='srarchIcon'></div>
             </div>
+            <SpFilter>
+
+            </SpFilter>
             <div className='specialty'>
                 {SpData.map(data => (
                     <SpecialtyItem key={data.speciality_code} name={data.speciality_name} class={data.class}
@@ -37,20 +46,41 @@ const Specialty = (props) => {
                     />
                 ))}
 
-                {/* <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음","아닌가?","몰라용"]}  />
-                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음","아닌가?","몰라용"]}  />
-                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음","아닌가?","몰라용"]}  />
-                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음","아닌가?","몰라용"]}  />
-                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음","아닌가?","몰라용"]}  />
-                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음","아닌가?","몰라용"]}  />
-                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음","아닌가?","몰라용"]}  />
-                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음","아닌가?","몰라용"]}  />
-                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음","아닌가?","몰라용"]}  />
-                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음","아닌가?","몰라용"]}  />
-                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음","아닌가?","몰라용"]}  /> */}
-                
+                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음", "아닌가?", "몰라용"]} />
+                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음", "아닌가?", "몰라용"]} />
+                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음", "아닌가?", "몰라용"]} />
+                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음", "아닌가?", "몰라용"]} />
+                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음", "아닌가?", "몰라용"]} />
+                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음", "아닌가?", "몰라용"]} />
+                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음", "아닌가?", "몰라용"]} />
+                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음", "아닌가?", "몰라용"]} />
+                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음", "아닌가?", "몰라용"]} />
+                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음", "아닌가?", "몰라용"]} />
+                <SpecialtyItem name="화생방병" class="전문" desc="이것은 소개입니다 이것은 소개입니다 이것은 소개입니다" military_kind="해군" tags={["휴가많음", "아닌가?", "몰라용"]} />
             </div>
         </>
+    );
+};
+
+const useStyles = makeStyles((theme) => ({
+    label: {
+        fontSize: theme.Typography.pxToRem(19),
+    }
+}));
+
+const SpFilter = (props) => {
+    const classes = useStyles();
+
+    return (
+        <fieldset className="SpFilter">
+            <legend style={{ fontSize: "1em", fontWeight: 600 }}>Filter</legend>
+            <FormGroup classes="FormGroup">
+                <FormControlLabel control={<Checkbox defaultChecked size="small" />} label="육군" classes={classes} />
+                <FormControlLabel control={<Checkbox defaultChecked size="small" />} label="해군" />
+                <FormControlLabel control={<Checkbox defaultChecked size="small" />} label="공군" />
+                <FormControlLabel control={<Checkbox defaultChecked size="small" />} label="해병대" />
+            </FormGroup>
+        </fieldset>
     );
 };
 
