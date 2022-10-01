40407c94ed56a8bb185b51d8e61c6a469a9bbe2f
Everdu <64959010+kckc0608@users.noreply.github.com>
2022-10-01 14:05:10 +0000

    router

--- WEB(BE)/app.js
+++ WEB(BE)/app.js
@@ -1,8 +1,8 @@
-import { fileURLToPath } from 'url';
 import express from 'express';
+import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
 import path from "path";
 import cors from "cors";
-import bodyParser from 'body-parser';
 
 import { router_speciality } from './router/speciality.js';
 import { router_auth } from './router/auth.js';
@@ -16,15 +16,16 @@ const port = 5000;
 app.use(bodyParser.urlencoded());
 app.use(express.json());
 app.use(cors());
+
+// 프론트 연동
 app.use(express.static(path.join(__dirname,"../WEB(FE)/mogoon/build")));
-
-
-app.use('/api/speciality', router_speciality);
-app.use('/api/auth', router_auth);
-
 app.get('/', (req, res) => {
     return res.sendFile(path.join(__dirname,"../WEB(FE)/mogoon/build/index.html"));
 });
+
+// API 라우팅
+app.use('/api/speciality', router_speciality);
+app.use('/api/auth', router_auth);
 
 app.listen(port, () => {
     console.log(`server is listening at ${port}`);
--- WEB(BE)/router/speciality.js
+++ WEB(BE)/router/speciality.js
@@ -1,16 +1,27 @@
 import express from 'express';
 import { db } from '../firebase/db.js';
-import { collection, getDocs, doc, setDoc } from 'firebase/firestore/lite';
+import { collection, getDocs, doc, setDoc, query, where } from 'firebase/firestore/lite';
 
 export const router_speciality = express.Router();
 
 router_speciality.get('/list', async (req, res) => {
-    const test_col = collection(db, 'speciality');
-    const snapshot = await getDocs(test_col);
+    const speciality_collection = collection(db, 'speciality');
+    const snapshot = await getDocs(speciality_collection);
     const doc_list = snapshot.docs.map(doc => doc.data());
     
     //await setDoc(doc(db, 'speciality', `test${doc_list.length}`), doc_list[0]);
     return res.json(doc_list);
 });
 
-router_speciality.get('/list')
+router_speciality.get('/list/:speciality_name', async (req, res) => {
+    const speciality_name = req.params.speciality_name;
+    const speciality_collection = collection(db, 'speciality');
+    const q = query(speciality_collection, where("speciality_name", "==", speciality_name));
+    const snapshot = await getDocs(q);
+    const doc_list = snapshot.docs.map(doc => doc.data());
+
+    if (doc_list.length == 0) {
+    }
+
+    return res.json(doc_list);
+});
