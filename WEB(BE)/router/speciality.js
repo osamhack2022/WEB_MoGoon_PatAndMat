a9d8dca5ff6a3de23b4b736aecc0cf2c3bf19b25
Everdu <64959010+kckc0608@users.noreply.github.com>
2022-10-05 04:49:49 +0000

    bug fix

--- WEB(BE)/router/speciality.js
+++ WEB(BE)/router/speciality.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import { ctrl_speciality } from '../controller/ctrl.speciality.js';
 
+import { fileURLToPath } from 'url';
+import path from "path";
+
 export const router_speciality = express.Router();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 router_speciality.get('/list', ctrl_speciality.get.speciality_list);
 router_speciality.get('/list/:speciality_name', ctrl_speciality.get.speciality_desc);
 
 router_speciality.get('/add', (req, res) => {
-    res.sendFile('../view/add_speciality.html');
+    res.sendFile(path.join(__dirname,'../view/add_speciality.html'));
 });
