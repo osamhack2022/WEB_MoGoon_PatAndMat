716f79dba9d9960a452bf144cfe8d3f7a7c06319
Everdu <64959010+kckc0608@users.noreply.github.com>
2022-10-05 04:46:22 +0000

    test page

--- WEB(BE)/router/speciality.js
+++ WEB(BE)/router/speciality.js
@@ -5,3 +5,7 @@ export const router_speciality = express.Router();
 
 router_speciality.get('/list', ctrl_speciality.get.speciality_list);
 router_speciality.get('/list/:speciality_name', ctrl_speciality.get.speciality_desc);
+
+router_speciality.get('/add', (req, res) => {
+    res.sendFile('../view/add_speciality.html');
+});
