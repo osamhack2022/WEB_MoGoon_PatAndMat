964f53ae7d4e6361040e07d572f02b063d6ea480
WBCode3 <80467790+WBCode3@users.noreply.github.com>
2022-10-09 14:44:44 +0000

    UPDATE : Enlist slide

--- WEB(FE)/mogoon/src/components/Enlist/Enlist.js
+++ WEB(FE)/mogoon/src/components/Enlist/Enlist.js
@@ -5,56 +5,68 @@ import "../../css/Enlist.css"
 let Enlist = (props) => {
     const slideRef = useRef();
     const [slideindex, setSlideindex] = useState(1);
+    const [btnstyle, setbtnstyle] = useState();
 
-    const handelpre =()=>{
-        console.log(slideRef.current.offsetWidth);
-        setSlideindex(slideindex-1);
-        slideRef.current.style.transform = `translateX(+${slideRef.current.offsetWidth * slideindex}px)`;
+    const handelpre = () => {
+        let t1 = slideindex - 1
+        setSlideindex(t1);
+        slideRef.current.style.transform = `translateX(-${slideRef.current.offsetWidth / 4 * (t1 - 1)}px)`;
     }
 
-    const handelnext =()=>{
-        setSlideindex(slideindex+1);
-        console.log(slideRef.current.offsetWidth * slideindex);
-        slideRef.current.style.transform = `translateX(-${slideRef.current.offsetWidth * slideindex}px)`;
+    const handelnext = () => {
+        let t2 = slideindex + 1;
+        setSlideindex(t2);
+        slideRef.current.style.transform = `translateX(-${slideRef.current.offsetWidth / 4 * slideindex}px)`;
+    }
+
+    let stSteps = ["군종을 선택해주세요.", "특기를 선택해주세요.", "개인정보를 입력해주세요.", "서류 점수 확인"];
+
+    let StepItem = () => {
+
+        return (
+            stSteps.map((item, index) => (
+                <div>
+                    <div className={slideindex === (index + 1) ? "steps-index-active" : "steps-index"}>{index + 1}</div>
+                    <span style={{ marginRight: "20px" }}>{item}</span>
+                    <hr className={`steps-hr${index}`} />
+                </div>
+            ))
+        );
     }
 
     return (
         <div className='Enlist-wrap'>
-            <button className='btnpre' onClick={handelpre}>이전</button>
-            <button className='btnnext' onClick={handelnext}>다음</button>
+            <button className={slideindex === 1 ? "btnblock" : "btnpre"} onClick={handelpre}>이전</button>
+            <button className={slideindex === 4 ? "btnblock" : "btnnext"} onClick={handelnext}>다음</button>
             <div className='stepper-wrap'>
                 <StepItem />
             </div>
-            <div className='stepper-content' name={slideindex} ref={slideRef}>
+            <div className='stepper-content' ref={slideRef}>
                 <div className='stepper-content-inner'>
-
+                    <Content1/>
                 </div>
                 <div className='stepper-content-inner'>
-
+                    {/* <Content1/> */}
                 </div>
                 <div className='stepper-content-inner'>
-
+                    안녕하세요2
                 </div>
                 <div className='stepper-content-inner'>
-
+                    안녕하세요3
                 </div>
             </div>
         </div>
     );
 };
 
-let stSteps = ["군종을 선택해주세요.", "특기를 선택해주세요.", "개인정보를 입력해주세요.", "서류 점수 확인"];
+const Content1 = () => {
 
-let StepItem = () => {
+    return(
+        <div className='content1-wrap'>
+            <div>   
 
-    return (
-        stSteps.map((item, index) => (
-            <div>
-                <div className='steps-index'>{index + 1}</div>
-                <span style={{ marginRight: "20px" }}>{item}</span>
-                <hr className={`steps-hr${index}`} />
             </div>
-        ))
+        </div>
     );
 }
 
