1fa739712433d15b29bdc84aa3ded35f4e44f7c9
Everdu <64959010+kckc0608@users.noreply.github.com>
2022-10-23 09:56:34 +0000

    FEAT : 마이페이지 지원 정보 수정 API 추가

--- WEB(BE)/router/user.js
+++ WEB(BE)/router/user.js
@@ -5,3 +5,4 @@ import { ctrl_user } from '../controller/ctrl.user.js';
 
 export const router_user = express.Router();
 router_user.get('/info', ctrl_user.get.user_info);
+router_user.put('/en-info/:info_type', ctrl_user.put.update_user_en_info);
